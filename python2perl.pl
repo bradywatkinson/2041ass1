e5c8aa20246d3915339d73ec3cc24554db6c7207
Brady Watkinson <dabradybunch1@hotmail.com>
2014-09-22 16:58:08 +1000

    coding: added variables with operator assignment

--- python2perl.pl
+++ python2perl.pl
@@ -6,23 +6,32 @@
 while ($line = <>) {
 	if ($line =~ /^#!/ && $. == 1) {		
 		print "#!/usr/bin/perl -w\n";
-		
-	} elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {		
+	
+#===blank lines===
+	} elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {	
 		print $line;
-		
+	
+#===printing===
 	} elsif ($line =~ /^\s*print\s*"(.*)"\s*$/) {		
 		print "print \"$1\\n\";\n";
 		
 	} elsif ($line =~ /^\s*print\s*(.*)\s*$/) {		
 		print "print \$$1\,\ \"\\n\";\n";
 
+#===variables===
+	#strings
 	} elsif ($line =~ /^\s*(\w+)\s*=\s*(\w+)\s*$/) {
 		print "\$$1 = $2;\n";
-
-	} else {
 	
-		# Lines we can't translate are turned into comments
-		
-		print "#$line\n";
+	} elsif ($line =~ /^\s*(\w+)\s*=\s*([\d\ \*\+\/-]+)\s*$/) {
+		print "\$$1 = $2;\n";		
+
+
+#===else comment out things===
+	} else {	
+		chomp $line;
+		print "#$line# <====this line was not converted\n";
 	}
 }
+
+#	} elsif ($line =~ /^\s(.*)\s*$/)
